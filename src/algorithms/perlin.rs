c70cc1146b1ebc3fd3c761a8e08c30b454844c01
Lars <l.danger@web.de>
2024-05-09 08:28:23 +0200

    wip

--- src/algorithms/perlin.rs
+++ src/algorithms/perlin.rs
@@ -6,6 +6,18 @@ pub struct PerlinParameters {
     pub scale: f32,
 }
 
-pub fn perlin_2d(x: usize, y: usize, params: PerlinParameters) -> f32 {
-    1.0
+pub fn perlin_2d(
+    x: u32,
+    y: u32,
+    params: PerlinParameters,
+) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let mut imgbuf = image::ImageBuffer::new(x, y);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.8 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+
+    imgbuf
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod algorithms {
     pub mod perlin;
 }
 
-use crate::algorithms::perlin::PerlinParameters;
+use crate::algorithms::perlin;
 
 fn less_than_3(s: &str) -> Result<u8, String> {
     number_range(s, 0, 3)
@@ -20,17 +20,22 @@ struct Cli {
     #[arg(short, long, value_parser=less_than_3, default_value_t=2)]
     dimension: u8,
     // The path to the outputfile
-    // path: std::path::PathBuf,
+    #[arg(short, long)]
+    path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
+
     println!("Hello, world! {:?}", args);
-    let x = PerlinParameters {
+    let x = perlin::PerlinParameters {
         seed: 1,
         scale: 1.0,
         octaves: 1,
         persistence: 1.0,
     };
-    println!("Hello, world! {:?}", x);
+
+    let out = perlin::perlin_2d(800, 800, x);
+
+    out.save(args.path.as_path()).unwrap();
 }
